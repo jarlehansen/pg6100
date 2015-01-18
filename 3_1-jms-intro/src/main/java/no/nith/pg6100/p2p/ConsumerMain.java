6331cedd8581f4b31c237748257602dec0837da6
jarlehansen <jarle@jarlehansen.net>
2015-01-18 22:11:16 +0100

    oppdatert

---
+++ 3_1-jms-intro/src/main/java/no/nith/pg6100/p2p/ConsumerMain.java
@@ -0,0 +1,17 @@
+package no.nith.pg6100.p2p;
+
+import javax.jms.JMSException;
+
+public class ConsumerMain {
+
+    public void sendMessage() {
+
+    }
+
+    public void receiveMessage() {
+
+    }
+
+    public static void main(String[] args) throws JMSException {
+    }
+}
---
+++ 3_1-jms-intro/src/main/java/no/nith/pg6100/p2p/ProducerMain.java
@@ -0,0 +1,5 @@
+package no.nith.pg6100.p2p;
+
+public class ProducerMain {
+
+}
