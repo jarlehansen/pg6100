30ab881dc27adf16ada12210f764712684a3999c
jarlehansen <jarle@jarlehansen.net>
2015-01-26 10:13:37 +0100

    jms

---
+++ 3_4-jms-egentrening/src/main/java/no/nith/pg6100/jms/PG6100Queue.java
@@ -0,0 +1,16 @@
+package no.nith.pg6100.jms;
+
+import javax.enterprise.util.Nonbinding;
+import javax.inject.Qualifier;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.TYPE, ElementType.PARAMETER})
+public @interface PG6100Queue {
+    @Nonbinding
+    String value() default "";
+}
--- 3_4-jms-egentrening/src/main/java/no/nith/pg6100/jms/PG6100RemoteConnectionFactory.java
+++ 3_4-jms-egentrening/src/main/java/no/nith/pg6100/jms/PG6100RemoteConnectionFactory.java
@@ -1,9 +1,12 @@
 package no.nith.pg6100.jms;
 
+import javax.annotation.PreDestroy;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
 import javax.jms.ConnectionFactory;
+import javax.jms.JMSContext;
+import javax.jms.Queue;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -13,22 +16,45 @@ import java.util.Properties;
 @ApplicationScoped
 public class PG6100RemoteConnectionFactory {
 
-    private ConnectionFactory connectionFactory = null;
+    private JMSContext context = null;
+    private Queue queue = null;
 
     @Produces
     @PG6100ConnectionFactory
-    ConnectionFactory connectionFactory(InjectionPoint injectionPoint) throws IOException, NamingException {
-        if (connectionFactory == null) {
+    public JMSContext context(InjectionPoint injectionPoint) throws IOException, NamingException {
+        if (context == null) {
             PG6100ConnectionFactory annotation = injectionPoint.getAnnotated().getAnnotation(PG6100ConnectionFactory.class);
             String providerUrl = annotation.value();
 
             Properties properties = new Properties();
             properties.put(Context.INITIAL_CONTEXT_FACTORY, org.jboss.naming.remote.client.InitialContextFactory.class.getName());
             properties.put(Context.PROVIDER_URL, System.getProperty(Context.PROVIDER_URL, providerUrl));
+            properties.put(Context.URL_PKG_PREFIXES,  "org.jboss.ejb.client.naming");
 
             InitialContext initialContext = new InitialContext(properties);
-            connectionFactory = (ConnectionFactory) initialContext.lookup("jms/RemoteConnectionFactory");
+            ConnectionFactory connectionFactory = (ConnectionFactory) initialContext.lookup("jms/RemoteConnectionFactory");
+            context = connectionFactory.createContext();
         }
-        return connectionFactory;
+        return context;
+    }
+
+    @Produces
+    @PG6100Queue
+    public Queue queue(InjectionPoint injectionPoint) {
+        if (queue == null) {
+            PG6100Queue annotation = injectionPoint.getAnnotated().getAnnotation(PG6100Queue.class);
+            String queueName = annotation.value();
+            if (queueName == null || queueName.length() == 0)
+                throw new IllegalArgumentException(String.format("%s must specify queue name", PG6100Queue.class.getSimpleName()));
+
+            queue = context.createQueue(queueName);
+        }
+
+        return queue;
+    }
+
+    @PreDestroy
+    public void closeContext() {
+        context.close();
     }
 }
