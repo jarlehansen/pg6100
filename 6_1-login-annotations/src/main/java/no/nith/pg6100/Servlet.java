82d07695b9e65f676574be7904c26078dba1e841
jarlehansen <jarle@jarlehansen.net>
2015-02-10 22:34:49 +0100

    login

---
+++ 6_1-login-annotations/src/main/java/no/nith/pg6100/Servlet.java
@@ -0,0 +1,27 @@
+package no.nith.pg6100;
+
+import javax.annotation.security.DeclareRoles;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.HttpConstraint;
+import javax.servlet.annotation.ServletSecurity;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/servlet")
+@DeclareRoles("pg6100_user")
+@ServletSecurity(
+        value = @HttpConstraint(rolesAllowed = {"pg6100_user"})
+)
+public class Servlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try (PrintWriter writer = resp.getWriter()) {
+            writer.println("Test");
+        }
+    }
+}
