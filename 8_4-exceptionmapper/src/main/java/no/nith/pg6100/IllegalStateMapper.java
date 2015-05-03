ef9ffd0080f4590a456e6e41f7b3c4464722c014
jarlehansen <jarle@jarlehansen.net>
2015-05-03 23:26:33 +0200

    exception mapper

--- 8_4-exceptionmapper/src/main/java/no/nith/pg6100/IllegalStateMapper.java
+++ 8_4-exceptionmapper/src/main/java/no/nith/pg6100/IllegalStateMapper.java
@@ -6,9 +6,9 @@ import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
 @Provider
-public class IllegalStateMapper implements ExceptionMapper<IllegalStateException> {
+public class IllegalStateMapper implements ExceptionMapper<RuntimeException> {
     @Override
-    public Response toResponse(IllegalStateException exception) {
+    public Response toResponse(RuntimeException exception) {
         return Response.status(404).entity(exception.getMessage()).type(MediaType.TEXT_PLAIN).build();
     }
 }
